dd5a2ef9008d61377bde286d6550df72d71c332b
ertug-guney <yukselenay@gmail.com>
2022-04-16 23:14:07 +0300

    package.json added

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 import getData from "./getData.js";
 
 getData(1); //1. yöntem
-
-// getData(1).then((res) => console.log(res)); // 2.1 yöntem
-
-// getData(1).then(console.log);  // 2.2 yöntem
--- getData.js
+++ getData.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 export default async function getData(user_id) {
-  const { data: user } = await axios(
+  const { data: users } = await axios(
     "https://jsonplaceholder.typicode.com/users/" + user_id
   );
 
-  const { data: post } = await axios(
+  const { data: posts } = await axios(
     "https://jsonplaceholder.typicode.com/posts?userId=" + user_id
   );
-  console.log(user); //1. yöntem
-  console.log(post); //1. yöntem
-  // return { data: user }, { data: post }; //2. yöntem
+  console.log(users); //1. yöntem
+  console.log(posts); //1. yöntem
 }
