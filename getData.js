d28d985b383d2b16907156b29fceb18fedd07c6c
ertug-guney <yukselenay@gmail.com>
2022-04-16 23:40:54 +0300

    coming 1 post code added

--- getData.js
+++ getData.js
@@ -7,7 +7,12 @@ export default async function getData(user_id) {
 
   const { data: posts } = await axios(
     "https://jsonplaceholder.typicode.com/posts?userId=" + user_id
-  );
+  ); //ödevdeki link kullanılırsa (?userId=) 10 tane post gelir
+
+  // const { data: posts } = await axios(
+  //   "https://jsonplaceholder.typicode.com/posts/" + user_id
+  // ); // 1 post gelmesi için
+
   console.log(users); //1. yöntem
   console.log(posts); //1. yöntem
 }
